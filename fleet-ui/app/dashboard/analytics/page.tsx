ad4976d8a0ee2cf6abd00641c43c1c6e7d1bfd1c
Anurag Borkar <anuragborkar200@gmail.com>
2026-05-19 22:21:37 +0000

    add grafana dashboard

--- fleet-ui/app/dashboard/analytics/page.tsx
+++ fleet-ui/app/dashboard/analytics/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
-  return <div>Analytics</div>;
+export default function AnalyticsPage() {
+  return (
+    <div className="p-4 h-screen w-full">
+      <iframe
+        src="http://localhost:3001/d/g95qrz/linux-fleet-dashboard?orgId=1&kiosk&theme=light"
+        className="w-full h-full rounded-md border border-gray-200 shadow-sm"
+        frameBorder="0"
+        allowFullScreen
+      />
+    </div>
+  );
 }
